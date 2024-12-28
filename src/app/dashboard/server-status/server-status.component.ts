2987e195c892730439820d5b47a215e970547361
LeelaChoudary <leelachoudary3@gmail.com>
2024-12-28 10:42:27 +0530

    signal effect changes commit

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, effect, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'offline';
+export class ServerStatusComponent implements OnInit{
+  currentStatus = signal<'online' | 'offline' | 'unknown'>('offline');
+  constructor(){
+    effect(()=>{console.log(this.currentStatus())})  //signal effect is used to set subscription on signal change.
+  }
+  ngOnInit(){
+    console.log("NG ONINIT")
+    const interval=setInterval(()=>{
+      const rnd=Math.random(); //0 - 0.9999
+      if(rnd<0.5){
+this.currentStatus.set('online');
+      }
+      else if(rnd<0.9){
+        this.currentStatus.set('offline');
+      }
+      else{
+        this.currentStatus.set('unknown');
+      }
+    }, 5000
+    )
+  }
 }
