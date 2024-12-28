3d29c69a2e3eb244efc41f3988f5ee9b16f26bfd
LeelaChoudary <leelachoudary3@gmail.com>
2024-12-28 13:19:18 +0530

    comments added

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -9,8 +9,9 @@ import { Ticket } from '../tickets.model';
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
-@Input({required:true}) ticketObj:Ticket | undefined 
-@Output() close=new EventEmitter();
+@Input({required:true}) ticketObj:Ticket | undefined   //for input ,2 configurations are available(alias,transform)
+@Output() close=new EventEmitter();                     //for output ,1 configuration is available(alias)
+                                              //alias helps us to give other name to input property which we can use outside of this component
 // isVisible=false;
 isVisible=signal(false);
 onButtonToggle(){
